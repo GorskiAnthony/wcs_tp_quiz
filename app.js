2d4ca2155de1b93d7635b90d5fffd55d1887578d
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-09-29 12:35:46 +0200

    correction

--- app.js
+++ app.js
@@ -22,3 +22,67 @@
  */
 
 /********************** Exercice **********************/
+
+/**
+ *
+ * Créations des tableaux pour les réponses
+ */
+const RESPONSES = ["c", "a", "c", "b", "c"];
+let checkResponses = [];
+let userResponses = [];
+
+// Get my form
+const FORM = document.querySelector(".form-quiz");
+
+// get info from dom
+const TITLE = document.querySelector(".resultats h2");
+const AIDE = document.querySelector(".aide");
+const NOTE = document.querySelector(".note");
+
+// Listen to the submit event
+FORM.addEventListener("submit", (event) => {
+  event.preventDefault();
+  for (let i = 1; i <= RESPONSES.length; i++) {
+    userResponses.push(
+      document.querySelector(`input[name=q${i}]:checked`).value
+    );
+  }
+
+  //console.log(userResponses);
+  checkIsTrue(userResponses);
+
+  userResponses = [];
+});
+
+function checkIsTrue(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === RESPONSES[i]) {
+      checkResponses.push(true);
+    } else {
+      checkResponses.push(false);
+    }
+  }
+
+  // display good responses
+  displayInfo(checkResponses);
+
+  console.log(checkResponses);
+  checkResponses = [];
+}
+
+function displayInfo(arr) {
+  const GOOD_RESPONSES = arr.filter((response) => response === true).length;
+  if (GOOD_RESPONSES === 0) {
+    TITLE.innerHTML = "<bold>Tu m'as deçu</bold>";
+    AIDE.innerText = "Retourne travailler";
+    NOTE.innerText = "0/5";
+  } else if (GOOD_RESPONSES === 5) {
+    TITLE.innerHTML = "<bold>Tu es le meilleur !</bold>";
+    AIDE.innerText = "Tu peux prendre un repos";
+    NOTE.innerText = "5/5";
+  } else {
+    TITLE.innerHTML = "<bold>Dommage, tu y es presque !</bold>";
+    AIDE.innerText = "Allez, encore un peu";
+    NOTE.innerText = `${GOOD_RESPONSES}/5`;
+  }
+}
